5ef246725e816e37c2739b087490065e5bb5e759
richard-kim-79 <blueslap0701@gmail.com>
2025-04-16 16:03:37 +0900

    Initial commit: Remove Discord notification feature

--- src/components/AlertSystem.tsx
+++ src/components/AlertSystem.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import { BellIcon } from '@heroicons/react/24/outline';
 
--- src/components/MonitoringDashboard.tsx
+++ src/components/MonitoringDashboard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
